78e4b28bb9e5d23f25108d0afa9afbd6bfe7abda
cloudtonero <147879628+cloudtonero@users.noreply.github.com>
2023-11-15 17:42:38 +0100

    #1 created the modules directories and all relevant tf files for project (#2)
    Co-authored-by: Tony Osunde <112965272+SirTonero@users.noreply.github.com>

---
+++ bin/tfvar.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+project_root="/Users/mrtonero/Desktop/projects/terraform-s3-static-website-hosting"
+
+cp $project_root/terraform.tfvars.example $project_root/terraform.tfvars
